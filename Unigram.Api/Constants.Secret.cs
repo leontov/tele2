ab6b7784abc14b8117f76425e8e5db08fdcf006e
владислав кочуров <cochu@DESKTOP-GBTAI8C>
2018-01-29 02:00:28 +0300

    Добавьте файлы проекта.

---
+++ Unigram.Api/Constants.Secret.cs
@@ -0,0 +1,11 @@
+namespace Telegram.Api
+{
+    public static partial class Constants
+    {
+        static Constants()
+        {
+            ApiId = 181442;
+            ApiHash = "b7dc6bad37a49e757df800c6db074dd3";
+        }
+    }
+}
